6375ade55ece7e2890abb27ef19b83e916f02dec
AbbasAkbar <abbasekber@gmail.com>
2020-10-12 21:25:25 +0400

    login page

---
+++ src/login/screen/Login.js
@@ -0,0 +1,25 @@
+import React,{Component} from 'react'
+import {View,Text,Image,StyleSheet} from 'react-native'
+import Header from '../component/Header'
+import SocialMedia from '../component/SocialMedia'
+import EmailLogin from '../component/EmailLogin'
+import LoginButton from '../component/LoginButton'
+
+export default class Login extends Component{
+    render(){
+        return(
+            <View style={styles.container}>
+                <Header />
+                <SocialMedia/>
+                <EmailLogin/>
+                <LoginButton/>
+            </View>
+        )
+    }
+}
+
+const styles=StyleSheet.create({
+    container:{
+        marginTop:'10%'
+    }
+})
